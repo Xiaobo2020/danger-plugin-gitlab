26680e5a5c10c675d360f4dd30a4e7b765c38381
Xiaobo Wang （王晓波） <xiaobo_wang@trip.com>
2023-04-21 19:58:16 +0800

    chore: add checkMutexUpdate

--- src/libs/index.ts
+++ src/libs/index.ts
@@ -5,5 +5,6 @@ export { default as checkDescription } from "./checkDescription";
 export { default as checkIssue } from "./checkIssue";
 export { default as checkLockfile } from "./checkLockfile";
 export { default as checkManuallyTested } from "./checkManuallyTested";
+export { default as checkMutexUpdate } from "./checkMutexUpdate";
 export { default as checkSelfReview } from "./checkSelfReview";
 export { default as checkSize } from "./checkSize";
